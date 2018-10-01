af4cc2f24e5c97ce5412871a2ff9408153d9e421
Varun Varma <vvarun@yahoo-inc.com>
2018-10-01 14:20:57 -0700

    Build using tox

--- setup.py
+++ setup.py
@@ -151,7 +151,6 @@ setup(
             'Operating System :: OS Independent',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.6',
             'Topic :: System :: Networking',
             'Topic :: System :: Networking :: Monitoring'
         ]
